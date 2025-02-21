740028291bfbc88834dc2ee9a99cadaa437a9595
jungwoo-ryu <jryu1521@gmail.com>
2025-02-22 00:33:44 +0900

    image deblurring

--- lib/components/my_drawer.dart
+++ lib/components/my_drawer.dart
@@ -13,86 +13,83 @@ class MyDrawer extends StatelessWidget {
     auth.signOut();
   }
 
-
-
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      // backgroundColor: Theme.of(context).colorScheme,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Column(children: [
-            //Logo
-          DrawerHeader(
-            child: Center(
-              child: ImageIcon(
-              const AssetImage("assets/images/Bucky.png"),
-              size: 100,
-              color:Colors.red.shade400,
+          Column(
+            children: [
+              // Logo
+              DrawerHeader(
+                child: Center(
+                  child: Image.asset(
+                    'assets/images/Bucky.png',
+                    width: 100,
+                    fit: BoxFit.cover,
+                  ),
+                ),
               ),
+              // Home list tile
+              Padding(
+                padding: const EdgeInsets.only(left: 25.0),
+                child: ListTile(
+                  title: const Text("H O M E"),
+                  leading: const Icon(Icons.home),
+                  onTap: () {
+                    // pop the drawer
+                    Navigator.pop(context);
+                  },
+                ),
               ),
+              // Message list tile
+              Padding(
+                padding: const EdgeInsets.only(left: 25.0),
+                child: ListTile(
+                  title: const Text("M E S S A G E"),
+                  leading: const Icon(Icons.message),
+                  onTap: () {
+                    Navigator.pop(context);
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => MessengerPage()),
+                    );
+                  },
+                ),
+              ),
+              // Settings list tile
+              Padding(
+                padding: const EdgeInsets.only(left: 25.0),
+                child: ListTile(
+                  title: const Text("S E T T I N G S"),
+                  leading: const Icon(Icons.settings),
+                  onTap: () {
+                    // pop the drawer
+                    Navigator.pop(context);
+                    // navigate to settings page
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const SettingsPage(),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
           ),
-
-          // home list tile
-          Padding(
-            padding: const EdgeInsets.only(left: 25.0),
-            child: ListTile(
-              title: const Text("H O M E"),
-              leading: const Icon(Icons.home),
-              onTap: () {
-                // pop the drawer
-                Navigator.pop(context);
-              },
-            ),
-          ),
-          // message list tile
-          Padding(
-            padding: const EdgeInsets.only(left: 25.0),
-            child: ListTile(
-              title: const Text("M E S S A G E"),
-              leading: const Icon(Icons.message),
-              onTap: () {
-                Navigator.pop(context);
-
-                Navigator.push(context, MaterialPageRoute(builder: (context) => MessengerPage()));
-              },
-            ),
-          ),
-
-
-          // settings list tile
-          Padding(
-            padding: const EdgeInsets.only(left: 25.0),
-            child: ListTile(
-              title: const Text("S E T T I N G S"),
-              leading: const Icon(Icons.settings),
-              onTap: () {
-                // pop the drawer
-                Navigator.pop(context);
-                
-                // navigate to settings page
-                Navigator.push(context, MaterialPageRoute(builder: 
-                (context) => const SettingsPage(),));
-              },
-            ),
-          ),
-          ],),
-
-          // logout list tile
+          // Logout list tile
           Padding(
             padding: const EdgeInsets.only(left: 25.0, bottom: 25.0),
             child: ListTile(
               title: const Text("L O G O U T"),
               leading: const Icon(Icons.logout),
               onTap: logout,
-
             ),
           ),
-
         ],
       ),
-
     );
   }
 }

ceb44f6ea5d5159465d9958b9cb6ecd27a15706e
jungwoo-ryu <jryu1521@gmail.com>
2025-03-13 22:04:53 +0900

    bottom nav bar added

--- lib/page/home_screen.dart
+++ lib/page/home_screen.dart
@@ -1,10 +1,10 @@
 import 'package:badger_market/DTO/product.dart';
+import 'package:badger_market/page/messnager_page.dart';
 import 'package:badger_market/page/post_product_screen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 import '../DTO/product_tile.dart';
-import '../components/my_drawer.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -17,6 +17,7 @@ class _HomeScreenState extends State<HomeScreen> {
   late String searchString;
   List<Product> products = [];
   bool isLoading = true;
+  int _selectedIndex = 0;
 
   @override
   void initState() {
@@ -52,6 +53,7 @@ class _HomeScreenState extends State<HomeScreen> {
     }
   }
 
+
   void setSearchString(String value) => setState(() {
         searchString = value;
       });
@@ -62,6 +64,12 @@ class _HomeScreenState extends State<HomeScreen> {
       MaterialPageRoute(builder: (context) => PostProductScreen()),
     );
     fetchProducts(); // Refresh the products when returning from the PostProductScreen
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 
   @override
@@ -79,45 +87,94 @@ class _HomeScreenState extends State<HomeScreen> {
     } else {
       searchResultTiles = products.map((p) => ProductTile(product: p)).toList();
     }
-    return Scaffold(
-      appBar: AppBar(
-        title: Container(
-          width: MediaQuery.of(context).size.width * 0.7, // Adjust the width as needed
-          height: MediaQuery.of(context).size.height * 0.05, // Adjust the height as needed 
-          child: SearchBar(
-            onChanged: setSearchString,
-          ),
-        ),
-        actions: const [
-          // CartAppBarAction(),
-        ],
-        backgroundColor: const Color.fromRGBO(161, 32, 43, 1),
-        iconTheme: IconThemeData(color: Colors.white), // Set hamburger button color to white
-      ),
-      drawer: const MyDrawer(),
-      body: isLoading
-          ? Center(child: CircularProgressIndicator())
-          : RefreshIndicator(
-              onRefresh: fetchProducts,
-              child: searchResultTiles.isNotEmpty
-                  ? Padding(
-                      padding: listViewPadding,
-                      child: ListView(
-                        children: searchResultTiles,
-                      ),
-                    )
-                  : Center(
-                      child: Text(
-                        'No products found',
-                        // style: Theme.of(context).textTheme.headline6,
-                      ),
-                    ),
+
+    List<Widget> _pages = <Widget>[
+      Scaffold(
+        appBar: AppBar(
+          title: Container(
+            width: MediaQuery.of(context).size.width * 0.7, // Adjust the width as needed
+            height: MediaQuery.of(context).size.height * 0.05, // Adjust the height as needed 
+            child: SearchBar(
+              onChanged: setSearchString,
             ),
+          ),
+          actions: const [
+            // CartAppBarAction(),
+          ],
+          backgroundColor: const Color.fromRGBO(161, 32, 43, 1),
+          iconTheme: IconThemeData(color: Colors.white), // Set hamburger button color to white
+        ),
+        body: isLoading
+            ? Center(child: CircularProgressIndicator())
+            : RefreshIndicator(
+                onRefresh: fetchProducts,
+                child: searchResultTiles.isNotEmpty
+                    ? Padding(
+                        padding: listViewPadding,
+                        child: ListView(
+                          children: searchResultTiles,
+                        ),
+                      )
+                    : Center(
+                        child: Text(
+                          'No products found',
+                          // style: Theme.of(context).textTheme.headline6,
+                        ),
+                      ),
+              ),
+      ),
+      Scaffold(
+        appBar: AppBar(
+          title: Text('Wish List'),
+          backgroundColor: const Color.fromRGBO(161, 32, 43, 1),
+        ),
+        body: Center(child: Text('Wish List')),
+      ),
+      MessengerPage(
+      ),
+      Scaffold(
+        appBar: AppBar(
+          title: Text('Profile'),
+          backgroundColor: const Color.fromRGBO(161, 32, 43, 1),
+        ),
+        body: Center(child: Text('Profile')),
+      ),
+    ];
+
+    return Scaffold(
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: _pages,
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: _postProduct,
         child: const Icon(Icons.add),
         backgroundColor: const Color.fromRGBO(161, 32, 43, 1),
         foregroundColor: Colors.white, // Set "+" button color to white
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Wish List',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat),
+            label: 'Chat',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: const Color.fromRGBO(161, 32, 43, 1),
+        onTap: _onItemTapped,
+        type: BottomNavigationBarType.fixed,
       ),
     );
   }
